7b9a6673e8297167422baeb803c2cdfbaac23ad4
RT <romantupikovroman@gmail.com>
2024-04-14 23:15:16 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <main className="main">
+    <div className="questionPage_wrapper">
+      <h1 className="questionPage_heading">Тестирование</h1>
+      <form className="questionPage_container">
+        <fieldset className="questionPage_fieldset">
+          <Question :question="currentQuestion" @answer="updateProgress" />
+        </fieldset>
+      </form>
+        <ProgressBar :current-question="currentQuestionIndex" :total-questions="questions.length"/>
+    </div>
+  </main>
+</template>
+
+<script>
+import ProgressBar from "@/components/ProgressBar.vue";
+import Question from "@/components/Question.vue";
+export default {
+  components: {ProgressBar, Question},
+  data() {
+    return {
+      questions: [
+        {
+          text: 'Что такое операционная система?',
+          options: ['Это просто программа на компьютере, как и другие - Word или Chrome', 'Это показатель того, какой процессор используется на компьютере. Например, 32-битный или 64-битный', 'Это набор взаимосвязанных программ, осуществляющих управление компьютером и взаимодействие с пользователем', 'Нет такого понятия, есть понятие "файловая система"'],
+          correctAnswer: 'Это набор взаимосвязанных программ, осуществляющих управление компьютером и взаимодействие с пользователем'
+        },
+        {
+          text: 'Является ли Android операционной системой?',
+          options: ['Да, это такая же ОС, как и другие, просто для мобильных девайсов', 'Нет, операционные системы бывают только для ПК', 'Нет, Android это программа, которая ставится на операционную систему девайса. ОС на разных девайсах разные', 'Это домашняя страничка в настройках вашего браузера'],
+          correctAnswer: 'Да, это такая же ОС, как и другие, просто для мобильных девайсов'
+        }
+
+      ],
+      currentQuestionIndex: 0
+    }
+  },
+  computed: {
+    currentQuestion() {
+      return this.questions[this.currentQuestionIndex];
+
+    },
+
+  },
+  methods: {
+    updateProgress() {
+      this.currentQuestionIndex++;
+    }
+  }
+
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="progress_numbers">
+    <div>{{ currentQuestion  }} </div>
+    <div>{{ totalQuestions }}</div>
+  </div>
+    <progress class="progress_bar" :value="currentQuestion + 1" :max="totalQuestions" ></progress>
+
+</template>
+
+<script>
+export default {
+  props: ['totalQuestions', 'currentQuestion']
+};
+</script>
+
+<style scoped>
+.progress_numbers{
+  display: flex;
+  justify-content: space-between;
+}
+.progress_bar{
+  width: 100%;
+}
+</style>
